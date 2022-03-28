a38cfd642fd0da5ff9da2cf26587737697a5b835
Snadman <escarrera@email.neit.edu>
2022-03-28 09:49:51 -0400

    index 1.1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Greetings for now this will be your main website to navigate ethan. Please clean me up, thanks.</h1>
+
+    <ul>
+        <li>Week One</li>
+    </ul>
+</body>
+</html>
